62e6601930f7df33d3ae608d627ef1c89ad99d7b
Sergey <serg888@mail.ru>
2016-12-17 16:59:54 +0700

    Начало подготовлено. Настроен Томкат, Гит, Build, папки и базовый класс Main

---
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -0,0 +1,10 @@
+package ru.javawebinar.topjava;
+
+/**
+ * Created by Сергей on 17.12.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.format("Привет всем!");
+    }
+}
